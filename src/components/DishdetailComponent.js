29b06e38254c23b95bfb3dae4d9328fd58b1ea63
Param Siddharth <contact@paramsid.com>
2020-11-05 18:24:39 +0530

    Single Page Applications, part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle,
+	Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderComments({comments}) {
 	if (comments != null) {
@@ -45,15 +47,45 @@ const Dishdetail = props => {
 	if (props.dish != null) {
 		return (
 			<div className='container'>
+				<Breadcrumb>
+					<BreadcrumbItem>
+						<Link to='/menu'>Menu</Link>
+					</BreadcrumbItem>
+					<BreadcrumbItem active>
+						{props.dish.name}
+					</BreadcrumbItem>
+				</Breadcrumb>
+				<div className='col-12'>
+					<h3>{props.dish.name}</h3>
+					<hr/>
+				</div>
 				<div class='row'>
 					<RenderDish dish={props.dish}/>
-					<RenderComments comments={props.dish.comments}/>
+					<RenderComments comments={props.comments}/>
 				</div>
 			</div>
 		);
 	}
 	
-	return <div></div>;
+	return (
+		<div className='container'>
+			<Breadcrumb>
+				<BreadcrumbItem>
+					<Link to='/menu'>Menu</Link>
+				</BreadcrumbItem>
+				<BreadcrumbItem active>
+					Invalid Dish
+				</BreadcrumbItem>
+			</Breadcrumb>
+			<div className='col-12'>
+				<h3>Invalid Dish</h3>
+				<hr/>
+			</div>
+			<div class='row'>
+				Invalid Dish!
+			</div>
+		</div>
+	);
 };
 
 export default Dishdetail;
