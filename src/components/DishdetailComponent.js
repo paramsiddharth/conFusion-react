f38e0c739b2b385a6e5b87be118b0e87f516c574
Param Siddharth <contact@paramsid.com>
2020-11-26 08:59:25 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle,
 	Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 // Validators
 const minLength = len => val => val?.length >= len;
@@ -128,6 +129,24 @@ function RenderDish({dish}) {
 }
 
 const Dishdetail = props => {
+	if (props.isLoading) {
+		return (
+			<div className='container'>
+				<div className='row'>
+					<Loading />
+				</div>
+			</div>
+		);
+	} else if (props.errMess) {
+		return (
+			<div className='container'>
+				<div className='row'>
+					<h4>{props.errMess}</h4>
+				</div>
+			</div>
+		);
+	}
+
 	if (props.dish != null) {
 		return (
 			<div className='container'>
